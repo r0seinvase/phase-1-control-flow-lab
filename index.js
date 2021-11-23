f1f7e0f280b32dc3b4b0c4a22677074d82852f73
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-23 11:21:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet < 400) {
+    return 'This one is on me!';
+  }else if (1999 < feet && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else (feet > 2500) ;{
+    return 'No can do.';}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+}else if (city === 'Pittsburgh');{
+    return 'No go.';}
+  }
+  function switchOnCharmFromTip (tip) {
+    switch (tip) {
+      case 'generous':
+        return "Thank you so much."    
+      case 'not as generous':
+        return "Thank you."
+      default:
+        return "Bye."
+    }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
